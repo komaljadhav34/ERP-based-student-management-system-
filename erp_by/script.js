3ab5700cfa29d03dd97f174aac73e0b941735a6f
komaljadhav34 <166813635+komaljadhav34@users.noreply.github.com>
2026-04-30 23:07:58 +0530

    Delete erp_by directory

--- erp_by/script.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.getElementById("menu-toggle");
-const sidebar = document.querySelector(".sidebar");
-if (menuToggle) {
-  menuToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
-  });
-}
